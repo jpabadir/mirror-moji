d8d7c1ab69b493772dd04c5bd498e33fa3875721
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-01-18 15:10:06 -0800

    add basic frontend elements

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld
+      msg="'Keeping people fed is my only peace of mind now, and I turn the 6 upside down, it's a 9 now.'"
+    />
+    <textarea rows="4" cols="50"></textarea>
+    <button @click="analyze">Analyze</button>
+    <br />
+    <br />
+    <video autoplay="true" ref="videoElement">hey</video>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     HelloWorld
+  },
+  mounted() {
+    // var video = this.$el.querySelector("#videoElement");
+    var video = this.$refs.videoElement;
+    if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function(stream) {
+          video.srcObject = stream;
+        })
+        .catch(function(err0r) {
+          console.log("Something went wrong!", err0r);
+        });
+    }
+  },
+  methods: {
+    analyze() {
+      console.log("analyzing");
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
